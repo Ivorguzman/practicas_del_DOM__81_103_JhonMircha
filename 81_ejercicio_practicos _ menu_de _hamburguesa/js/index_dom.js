3f07f2ba71722ad99262cf868b069b7dee29de81
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-05-26 20:41:54 -0400

    respaldo 20052022

--- 81_ejercicio_practicos _ menu_de _hamburguesa/js/index_dom.js
+++
@@ -1,8 +0,0 @@
-import hamburgerMenu from "./menu_hamburgesa.js";
-
-
-const d = document;
-d.addEventListener('DOMContentLoaded', (e) => {
-    hamburgerMenu('.panel-btn', '.panel', '.menu a');//se invoca la funcion hamburgerMenu con los parametros que se le pasan ('.panel-btn', '.panel')que esta en el archivo menu_hamburgesa.js dentro del archivo menu_hamburgesa.js  al caragar el DOM 
-
-}); 
--- 82_ejercicio_practicos_relojDigital_alarma/js/index_dom.js
+++
@@ -1,8 +0,0 @@
-import hamburgerMenu from "./dom/menu_hamburgesa.js";
-import { digitalClock, alarm } from "./dom/reloj.js";   // destructuracion: importar solo una funcion de un modulo  
-
-const d = document;
-d.addEventListener('DOMContentLoaded', (e) => {
-    hamburgerMenu('.panel-btn', '.panel', '.menu a');//se invoca la funcion hamburgerMenu con los parametros que se le pasan ('.panel-btn', '.panel')que esta en el archivo menu_hamburgesa.js dentro del archivo menu_hamburgesa.js  al caragar el DOM 
-    digitalClock('#reloj', '#activar_reloj', '#desactivar_reloj');
-}); 
