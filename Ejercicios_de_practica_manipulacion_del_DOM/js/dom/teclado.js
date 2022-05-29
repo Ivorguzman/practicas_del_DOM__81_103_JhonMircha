d4271b817a4efa94327ac93fb84da529f2d7b4cb
Ivorguzman <66047248+Ivorguzman@users.noreply.github.com>
2022-05-29 03:30:44 -0400

    respaldo 20052022

--- Ejercicios_de_practica_manipulacion_del_DOM/js/dom/teclado.js
+++ Ejercicios_de_practica_manipulacion_del_DOM/js/dom/teclado.js
@@ -1,1 +1,19 @@
 const d = document;
+
+export function shortcuts(e) {
+    // console.log(e);
+    // console.log(e.type);
+    console.log(e.key);
+    // console.log(e.keyCode);
+    // console.log(e.ctrlKey);
+    // console.log(e.altKey);
+    console.log(e.shiftKey);
+    // console.log(e);
+    // console.log(e.altKey);
+
+    // if (e.key === 'c' && e.ctrlKey=== true) { alert('Presionaste las teclas Ctrl -> c') };
+    if (e.key === 'm' && e.altKey === true) { confirm('Presionaste las teclas Alt ->  m'); };
+    // if (e.key === 'p' && e.ctrlKey === true) { prompt('Presionaste las teclas ctrl -> p'); };
+
+    if (e.key === 'i' && e.ctrlKey === true && e.altKey === true) { confirm('Presionaste las teclas Ctrl + Alt  ->  i') };
+}
--- Ejercicios_de_practica_manipulacion_del_DOM/js/index_dom.js
+++ Ejercicios_de_practica_manipulacion_del_DOM/js/index_dom.js
@@ -1,9 +1,22 @@
 import hamburgerMenu from "./dom/menu_hamburgesa.js";
-import { digitalClock, alarm } from "./dom/reloj.js";   // destructuracion: importar solo una funcion de un modulo  
+import { digitalClock, alarm } from "./dom/reloj.js";    
+import { shortcuts } from "./dom/teclado.js";
 
 const d = document;
 d.addEventListener('DOMContentLoaded', (e) => {
     hamburgerMenu('.panel-btn', '.panel', '.menu a');
     digitalClock('#reloj', '#activar_reloj', '#desactivar_reloj');
     alarm('assets/alarma.mp3', '#activar_alarma', '#desactivar_alarma');
-}); 
+
+}
+); 
+
+d.addEventListener('keydown', (e) => {
+    shortcuts(e)
+})
+// d.addEventListener('keyup', (e) => {
+//     shortcuts(e)
+// }) 
+// d.addEventListener('keypress', (e) => {
+//     shortcuts(e)
+// })
